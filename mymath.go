998b509d174d79b55929df2be3e07109af935b35
As67gTg <44134375+As67gTg@users.noreply.github.com>
2024-09-15 22:52:34 +0300

    v3

--- mymath.go
+++ mymath.go
@@ -1,25 +1,31 @@
 package mymath
 
 import "math"
-
+//корень из числа
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+//округление до наим целого большего или равноего х
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
+//Округление до наиб целого меньшего или равного х
 func Floor(x float64) float64 {
 	return math.Floor(x)
 }
+//возведение х в степень у
 func Pow(x, y float64) float64 {
 	return math.Pow(x, y)
 }
+//максимальное число из пары
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
+//минимальнео число из пары
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
 }
+//модуль
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
